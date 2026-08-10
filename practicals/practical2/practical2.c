cfb5a01d9b02a6270f4307d3e1c1e0444b67edab
nikhilsangu-ux <sangunikhil45@gmail.com>
2026-08-10 20:22:24 +0530

    Add files via upload

---
+++ practicals/practical2/practical2.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+int main()
+{
+    int src, dest;
+    char buffer[1024];
+    int bytes;
+
+    src = open("input.txt", O_RDONLY);
+
+    if (src < 0)
+    {
+        printf("Cannot open source file\n");
+        return 1;
+    }
+
+    dest = open("output.txt", O_WRONLY | O_CREAT | O_TRUNC, 0644);
+
+    if (dest < 0)
+    {
+        printf("Cannot open destination file\n");
+        close(src);
+        return 1;
+    }
+
+    while ((bytes = read(src, buffer, sizeof(buffer))) > 0)
+    {
+        write(dest, buffer, bytes);
+    }
+
+    close(src);
+    close(dest);
+
+    printf("File copied successfully.\n");
+
+    return 0;
+}
